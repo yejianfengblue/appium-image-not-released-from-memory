b7cb43057c6574ebfe27ec21a982666a11524c13
Jian Feng Ye <yejianfengblue@163.com>
2020-09-01 18:33:15 +0800

    Remove irrelevant driver setting and record server log and heap snapshot

--- loop-find-image.py
+++ loop-find-image.py
@@ -9,15 +9,9 @@ caps = dict(
             deviceName=device,
             udid=udid,
             newCommandTimeout=3600,
-            noReset=True,
-            imageMatchThreshold=1,
-            checkForImageElementStaleness=False,
-            getMatchedImageResult=True
+            noReset=True
         )
 driver = webdriver.Remote('http://localhost:4723/wd/hub', caps)
-driver.update_settings({"getMatchedImageResult": True,
-"imageMatchThreshold" : 0.8
-})
 while True:
     try:
         driver.find_element_by_image('./okabe.png')
